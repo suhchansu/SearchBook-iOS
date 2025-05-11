2ad01f168fe4a1f8bf1789750c5dbc1a4ee84f76
coolwater.suh <coolwater.suh@navercorp.com>
2025-05-11 18:58:17 +0900

    SearchBar, TableView 추가

---
+++ SearchBook-iOS/Search/SearchTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchTableViewCell.swift
+//  SearchBook-iOS
+//
+//  Created by USER on 5/11/25.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- SearchBook-iOS/ViewController.swift
+++ SearchBook-iOS/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum Constant {
+        static let cellID = "SearchTableViewCell"
+        static let tableViewHeight: CGFloat = 100
+    }
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.register(UINib(nibName: Constant.cellID, bundle: nil), forCellReuseIdentifier: Constant.cellID)
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: Constant.cellID, for: indexPath) as? SearchTableViewCell {
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return Constant.tableViewHeight
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let keyword = searchBar.text else { return }
+        
+        searchBar.resignFirstResponder()
+    }
+}
