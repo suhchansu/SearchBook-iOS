0a27eda3e42b1ec63ac81a88a08c8904a25fcd3f
coolwater.suh <coolwater.suh@navercorp.com>
2025-05-11 19:46:52 +0900

    TableView pagination 추가

--- SearchBook-iOS/ViewController.swift
+++ SearchBook-iOS/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
+extension ViewController: UITableViewDataSource, UITableViewDelegate, UITableViewDataSourcePrefetching {
     // MARK: - UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return bookList.count
@@ -49,6 +49,35 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - UITableViewDataSourcePrefetching
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard let keyword = searchBar.text else { return }
+
+        for indexPath in indexPaths {
+            guard bookList.count - 1 == indexPath.row else { continue }
+            
+            let currentPage = bookList.count / 10
+            APIRequester.shared.searchBookList(keyword: keyword, page: currentPage + 1) { [weak self] in
+                switch $0 {
+                case .failure(let error):
+                    let alert = UIAlertController(title: "오류",
+                                                  message: error.localizedDescription,
+                                                  preferredStyle: .alert)
+                    let action = UIAlertAction(title: "확인", style: .default)
+                    alert.addAction(action)
+                    DispatchQueue.main.async { [weak self] in
+                        self?.present(alert, animated: true)
+                    }
+                case .success(let bookList):
+                    self?.bookList += bookList
+                    DispatchQueue.main.async { [weak self] in
+                        self?.tableView.reloadData()
+                    }
+                }
+            }
+        }
     }
 }
 
